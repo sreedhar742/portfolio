1c53f91bd720e2694ed18fdb0355a047457c3b70
sreedhar742 <psridhar742@gmail.com>
2024-09-21 13:36:47 +0530

    update setting.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['sreedhar742.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['portfolio-3-j33w.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
