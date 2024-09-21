ef37be9d8da1614b1309ca9f1220edac004c6f44
sreedhar742 <psridhar742@gmail.com>
2024-09-21 14:55:08 +0530

    added urls

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['portfolio-pkpa.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
+# ['portfolio-pkpa.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
