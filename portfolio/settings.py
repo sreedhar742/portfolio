0d3319e1eaec5280e554593a2bfe473c46c1d754
sreedhar742 <psridhar742@gmail.com>
2024-09-21 13:27:02 +0530

    update dependencies

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['chickenleaf.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['sreedhar742.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
